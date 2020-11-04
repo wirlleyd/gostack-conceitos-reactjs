1520be15f30158863380658bc45efb6bc0697fa5
Wirlley Delfino <wirlley_d@hotmail.com>
2020-11-04 13:27:53 -0300

    Finalizando desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(()=> {
+     api.get('/repositories').then(({ data }) => setProjects(data));
+    }
+  ,[])
+
   async function handleAddRepository() {
-    // TODO
+    const {data: project} = await api.post('/repositories', {
+      url: "https://github.com/desafioReactJS",
+      title: "ReactJS",
+      techs: ["React", "Node.js"],
+    });
+    setProjects([...projects, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    const newProjectList = projects.filter(project => project.id !== id);
+    setProjects(newProjectList);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {projects.map(project => 
+        <li key={project.id}>
+          {project.title}
+          <button onClick={() => handleRemoveRepository(project.id)}>
             Remover
           </button>
-        </li>
+        </li>)
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
